de901c009c3795ed20763afb942eb01255b01765
Adam Kara <kara.p.adam@gmail.com>
2026-03-22 21:38:25 +0100

    chore(setup): initial project setup with layout, theme, navigation and pages

---
+++ src/utils/navigation.ts
@@ -0,0 +1,8 @@
+export const pages = [
+  { href: "/", label: "Home" },
+  { href: "/about", label: "About Me" },
+  { href: "/education", label: "Education" },
+  { href: "/experience", label: "Experience" },
+  { href: "/projects", label: "Projects" },
+  { href: "/contact", label: "Contact" },
+];
