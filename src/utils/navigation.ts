cfa5ed19df78f8d1f102228d8ecedd3113a4b681
Adam Kara <kara.p.adam@gmail.com>
2026-03-24 14:51:46 +0100

    feat(content): add landing page hero and tiles for navigating with images and icons

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -2,13 +2,41 @@ interface Page {
   label: "Home" | "About Me" | "Education" | "Experience" | "Projects" | "Contact";
   href: string;
   color?: string;
+  bannerImage?: string;
+  bannerImagePosition?: string;
 }
 
 export const pages: Page[] = [
   { href: "/", label: "Home", color: "#ff6f61" },
-  { href: "/about", label: "About Me", color: "#4ecdc4" },
-  { href: "/education", label: "Education", color: "#45b7d1" },
-  { href: "/experience", label: "Experience", color: "#96ceb4" },
-  { href: "/projects", label: "Projects", color: "#feca57" },
-  { href: "/contact", label: "Contact", color: "#ff9ff3" },
+  {
+    href: "/about",
+    label: "About Me",
+    color: "#4ecdc4",
+    bannerImage: "/src/assets/images/lego.jpeg",
+  },
+  {
+    href: "/education",
+    label: "Education",
+    color: "#45b7d1",
+    bannerImage: "/src/assets/images/elte-ik-inside.jpeg",
+  },
+  {
+    href: "/experience",
+    label: "Experience",
+    color: "#96ceb4",
+    bannerImage: "/src/assets/images/andermatt-architecture.jpeg",
+  },
+  {
+    href: "/projects",
+    label: "Projects",
+    color: "#feca57",
+    bannerImage: "/src/assets/images/prezar-demo.jpeg",
+  },
+  {
+    href: "/contact",
+    label: "Contact",
+    color: "#ff9ff3",
+    bannerImage: "/src/assets/images/mailbox.jpeg",
+    bannerImagePosition: "25% 50%",
+  },
 ];
