68ee14473e4a812e5399523dcb26eb825a053858
Adam Kara <kara.p.adam@gmail.com>
2026-04-01 22:48:24 +0200

    refact(pages): merge Education into Experience and add other metadata

--- src/utils/pageData.ts
+++ src/utils/pageData.ts
@@ -1,57 +1,70 @@
 export interface PageInfo {
-  key: string;
-  label: "Home" | "About Me" | "Education" | "Experience" | "Projects" | "Contact";
+  key: "home" | "about" | "education" | "experience" | "projects" | "contact";
+  title: string;
+  short: string;
   href: string;
   color?: string;
   bannerImage?: string;
   bannerImagePosition?: string;
+  enabled?: boolean;
 }
 
-export const pages: PageInfo[] = [
+const allPages: PageInfo[] = [
   {
     key: "home",
     href: "/home",
-    label: "Home",
+    title: "Home",
+    short: "Home",
     color: "#ff6f61",
   },
   {
     key: "about",
     href: "/about",
-    label: "About Me",
-    // color: "#4ecdc4",
+    title: "About Me",
+    short: "About Me",
+    color: "#4ecdc4",
     bannerImage: "/images/lego.jpeg",
     bannerImagePosition: "25% 70%",
   },
   {
     key: "education",
     href: "/education",
-    label: "Education",
-    // color: "#45b7d1",
+    title: "Education",
+    short: "Education",
+    color: "#45b7d1",
     bannerImage: "/images/elte-ik-inside.jpeg",
     bannerImagePosition: "25% 55%",
+    enabled: false,
   },
   {
     key: "experience",
     href: "/experience",
-    label: "Experience",
-    // color: "#96ceb4",
+    title: "Education & Experience",
+    short: "Experience",
+    color: "#96ceb4",
     bannerImage: "/images/andermatt-architecture.jpeg",
     bannerImagePosition: "25% 40%",
   },
   {
     key: "projects",
     href: "/projects",
-    label: "Projects",
-    // color: "#feca57",
+    title: "Projects",
+    short: "Projects",
+    color: "#feca57",
     bannerImage: "/images/prezar-demo.jpeg",
     bannerImagePosition: "25% 25%",
   },
   {
     key: "contact",
     href: "/contact",
-    label: "Contact",
-    // color: "#ff9ff3",
+    title: "Contact",
+    short: "Contact",
+    color: "#ff9ff3",
     bannerImage: "/images/mailbox.jpeg",
     bannerImagePosition: "25% 50%",
   },
 ];
+
+const isEnabled = ({ enabled }: PageInfo) => enabled === undefined || enabled;
+
+export const pages = allPages.filter(isEnabled);
